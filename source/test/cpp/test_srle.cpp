37219e09e38b83693b611619d5ef13e1cdd9e2cb
jurgen.kluft <jurgen.kluft@gmail.com>
2026-04-28 08:36:45 +0800

    * cmui
    * srle; need unittests

---
+++ source/test/cpp/test_srle.cpp
@@ -0,0 +1,24 @@
+#include "ccore/c_target.h"
+
+#include "cmui/c_srle.h"
+
+#include "cunittest/cunittest.h"
+
+using namespace ncore;
+
+UNITTEST_SUITE_BEGIN(srle)
+{
+    UNITTEST_FIXTURE(encode)
+    {
+        UNITTEST_FIXTURE_SETUP() {}
+        UNITTEST_FIXTURE_TEARDOWN() {}
+
+        UNITTEST_TEST(analysis)
+        {
+
+
+        }
+
+    }
+}
+UNITTEST_SUITE_END
